6408cf0f1ee5865a17258fe636274187c0449633
VincentIsip <163068569+VincentIsip@users.noreply.github.com>
2024-05-07 13:12:45 +1200

    Making titles and signup, login html
    Making form using form template and titles for home, login, and signup pages

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing", user="Tim")
+    data = request.form
+    print (data)
+    return render_template("login.html", booLean="True")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout<p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+                
     return render_template("signup.html")

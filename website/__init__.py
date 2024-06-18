965c281d06d33863f8c23102d9ebc347389a09fd
VincentIsip <163068569+VincentIsip@users.noreply.github.com>
2024-06-18 12:07:35 +1200

    fixed code
    fixed models.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,9 +9,10 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'syzyg0~M'
-    app.config['SQLALCHEMY_DATABASEURI']= f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
+    
+    
     from .views import views
     from .auth import auth
     
@@ -20,8 +21,15 @@ def create_app():
     
     from .models import User, Note
     
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
-def create_database(app):
-    if not path('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
-
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,6 +15,7 @@ def login():
 @auth.route('/logout')
 def logout():
     return "<p>Logout<p>"
+
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -30,6 +34,10 @@ def signup():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for(auth.home))
                 
     return render_template("signup.html")

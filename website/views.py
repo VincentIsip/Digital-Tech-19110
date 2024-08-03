2ef924df11822ac7f27dbba771d2c160dc593376
VincentIsip <163068569+VincentIsip@users.noreply.github.com>
2024-08-03 23:14:55 +1200

    blog posts, comments, css, masthead

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Note, Post, User
+from .models import Note, Post, User, Comment
 from . import db
 import json
 
@@ -17,11 +17,34 @@ def home():
     return render_template("home.html", user=current_user, posts=posts)
 
 #notes
-@views.route("/notes")
+@views.route('/notes', methods=['GET', 'POST'])
 @login_required
 def notes():
-    posts = Post.query.all()
-    return render_template("notes.html", user=current_user, posts=posts)
+    if request.method == 'POST': 
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note) 
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("notes.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
 
 #blog
 @views.route("/create-post", methods=['GET', 'POST'])
@@ -49,7 +72,7 @@ def delete_post(id):
 
     if not post:
         flash("Post does not exist.", category='error')
-    elif current_user.id != post.id:
+    elif current_user.id != post.author:
         flash('You do not have permission to delete this post.', category='error')
     else:
         db.session.delete(post)
@@ -70,3 +93,38 @@ def posts(username):
 
     posts = Post.query.filter_by(author=user.id).all()
     return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
